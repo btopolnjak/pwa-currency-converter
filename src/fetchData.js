0c86b8f50edaa2e8745e4e70143f1b1a2ee0a232
btopolnjak <boristopolnjak@Boriss-MacBook-Pro.local>
2021-12-25 20:59:23 +0100

    updated

--- src/fetchData.js
+++ src/fetchData.js
@@ -3,10 +3,8 @@ export default
 async () => {
   let body;
   let resultsFromServer = await fetch('https://cors-anywhere.herokuapp.com/https://api.hnb.hr/tecajn/v1', {
-    mode: "cors",
     method: "GET",
     headers: {
-      "Access-Control-Allow-Origin": "*",
       "Content-Type": "application/json"
     },
     body: JSON.stringify(body)
