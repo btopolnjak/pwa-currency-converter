86c1a8e616a1ed1ea1e222e83b5b9867b3aa8afc
btopolnjak <boristopolnjak@Boriss-MacBook-Pro.local>
2021-12-25 15:44:15 +0100

    updated

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,7 +1,7 @@
 export default
 
 async () => {
-  let resultsFromServer = await fetch('https://api.hnb.hr/tecajn/v1')
+  let resultsFromServer = await fetch('https://api.hnb.hr/tecajn/v1', {mode: 'cors'})
   let serverResponse = resultsFromServer.json()
   return serverResponse;
 }
--- sw.js
+++ sw.js
@@ -1,34 +1,29 @@
-const staticCacheName = "site-static-v2";
+// const staticCacheName = "site-static-v2";
 
-const assets = [
-    "/",
-    "/index.html",
-    "/favicon.ico",
-    "/styles/styles.css",
-    "/images/didier-weemaels-unsplash-500px.jpg",
-    "/images/icon-512x512.png",
-    "/images/icon-384x384.png",
-    "/images/icon-256x256.png",
-    "/images/icon-192x192.png",
-];
+// const assets = [
+//     "/",
+//     "/index.html",
+//     "/favicon.ico",
+//     "/styles/styles.css",
+//     "/images/didier-weemaels-unsplash-500px.jpg",
+//     "/images/icon-512x512.png",
+//     "/images/icon-384x384.png",
+//     "/images/icon-256x256.png",
+//     "/images/icon-192x192.png",
+// ];
 
 self.addEventListener('install', evt => {
-    evt.waitUntil(
-      caches.open(staticCacheName).then((cache) => {
-        console.log('caching shell assets');
-        cache.addAll(assets);
-      })
-    );
+    // console.log('service worker installed');
 });
 
 self.addEventListener('install', evt => {
-    console.log('service worker installed');
+    // console.log('service worker installed');
 });
   
 self.addEventListener('activate', evt => {
-    console.log('service worker activated');
+    // console.log('service worker activated');
 });
 
 self.addEventListener('fetch', evt => {
-    console.log('fetch event', evt);
+    // console.log('fetch event', evt);
 });
