904178d0df313d6ee51a0485e1854b0f361f9159
btopolnjak <boristopolnjak@Boriss-MacBook-Pro.local>
2021-12-25 20:53:11 +0100

    updated

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,10 +1,17 @@
 export default
 
 async () => {
-  let resultsFromServer = await fetch('https://api.hnb.hr/tecajn/v1', {
-    mode: 'cors',
-    headers: {'Access-Control-Allow-Origin':'*'}
-  })
+  let body;
+  let resultsFromServer = await fetch('https://cors-anywhere.herokuapp.com/https://api.hnb.hr/tecajn/v1', {
+    mode: "cors",
+    method: "GET",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
+  });
+  console.log(resultsFromServer);
   let serverResponse = await resultsFromServer.json()
   return await serverResponse;
 }
