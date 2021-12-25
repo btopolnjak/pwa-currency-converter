76f4102199273fe251808b9d123d335570fc034e
btopolnjak <boristopolnjak@Boriss-MacBook-Pro.local>
2021-12-25 16:12:54 +0100

    updated

--- src/fetchData.js
+++ src/fetchData.js
@@ -2,7 +2,8 @@ export default
 
 async () => {
   let resultsFromServer = await fetch('https://api.hnb.hr/tecajn/v1', {
-    "Access-Control-Allow-Origin" : "*"
+    mode: 'cors',
+    headers: {'Access-Control-Allow-Origin':'*'}
   })
   let serverResponse = await resultsFromServer.json()
   return await serverResponse;
