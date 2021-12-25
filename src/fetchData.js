2490c49e0c5e8f45652a85b0ce1885f92a11da8c
btopolnjak <boristopolnjak@Boriss-MacBook-Pro.local>
2021-12-25 15:59:06 +0100

    updated

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,7 +1,10 @@
 export default
 
 async () => {
-  let resultsFromServer = await fetch('https://api.hnb.hr/tecajn/v1', {mode: 'no-cors'})
+  let resultsFromServer = await fetch('https://api.hnb.hr/tecajn/v1', {
+    mode:'cors',
+    method: 'GET',
+  })
   let serverResponse = resultsFromServer.json()
   return serverResponse;
 }
