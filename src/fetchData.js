898d45c57f9707028c70649bb588c6f50b044f46
btopolnjak <boristopolnjak@Boriss-MacBook-Pro.local>
2021-12-28 17:44:06 +0100

    fetch update

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,7 +1,7 @@
 export default
 
 async () => {
-  let resultsFromServer = await fetch('https://d43b0db87c8e.sn.mynetname.net');
+  let resultsFromServer = await fetch('https://d43b0db87c8e.sn.mynetname.net:8443');
   let serverResponse = await resultsFromServer.json();
   return await serverResponse;
 }
