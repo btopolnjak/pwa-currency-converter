53695fbab3b0327c43ebe3f11381e21de82f3c4d
btopolnjak <boristopolnjak@Boriss-MacBook-Pro.local>
2022-01-26 20:31:28 +0100

    fetch url update

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,7 +1,7 @@
 export default
 
 async () => {
-  let resultsFromServer = await fetch('https://d43b0db87c8e.sn.mynetname.net:8443/https://api.hnb.hr/tecajn/v1');
+  let resultsFromServer = await fetch('https://d43b0db87c8e.sn.mynetname.net:8443/hnb');
   let serverResponse = await resultsFromServer.json();
   return await serverResponse;
 }
